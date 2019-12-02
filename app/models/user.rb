da66701ba91bf504be957d46c62c864a5c042145
rtnxs <riotnexus@mail.ru>
2019-12-02 16:21:51 +0300

    change downcase validation on create only

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   validates_confirmation_of :password
 
   before_save :encrypt_password
-  before_validation :username_downcase!
+  before_validation :username_downcase!, on: :create
 
   def username_downcase!
     self.username.downcase! unless username.nil?
