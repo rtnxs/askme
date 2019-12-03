be399030b332e66febc5f7d03d90f4befc0bffb9
rtnxs <riotnexus@mail.ru>
2019-12-03 18:36:30 +0300

    add avatar, helper user_avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def user_avatar(user)
+    #user.avatar_url.present? ? user.avatar_url : asset_path 'avatar.jpg'
+
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
 end
