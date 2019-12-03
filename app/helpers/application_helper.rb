d9d0edd6ca734ccf9edab9ca37ea215c7769d644
rtnxs <riotnexus@mail.ru>
2019-12-03 19:15:25 +0300

    add inclinator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def inclinator(number, one, few, many)
+    ostatok = number % 10
+    return many if ostatok > 4 || number.between?(11, 14) || ostatok.zero?
+    return one if ostatok == 1
+    return few if ostatok.between?(2, 4)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,5 @@
 module UsersHelper
   def user_avatar(user)
-    #user.avatar_url.present? ? user.avatar_url : asset_path 'avatar.jpg'
-
-    if user.avatar_url.present?
-      user.avatar_url
-    else
-      asset_path 'avatar.jpg'
-    end
+    user.avatar_url.present? ? user.avatar_url : (asset_path 'avatar.jpg')
   end
 end
