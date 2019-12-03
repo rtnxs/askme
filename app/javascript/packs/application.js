52a8222fb20b19c180bff4847979440613e740d6
rtnxs <riotnexus@mail.ru>
2019-12-03 18:40:12 +0300

    add script slide text form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(function(){
+    $('#ask-button').click(function(){
+        $('#ask-form').slideToggle(300);
+        return false;
+    });
+});
