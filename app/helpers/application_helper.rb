2c1f468dda04437dfd5ef2330194047882861cbb
rtnxs <riotnexus@mail.ru>
2019-12-06 18:46:37 +0300

    added author to question

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+  def author(question)
+    if question.author.present?
+      link_to question.author.username, user_url(question.author)
+    else
+      'anon'
+    end
+  end
 end
