c058c355f00be85c71a058b3140ab4308b636908
rtnxs <riotnexus@mail.ru>
2019-12-08 21:47:27 +0300

    refactore migration

--- db/migrate/20191207134857_change_column_default_user.rb
+++ db/migrate/20191207134857_change_column_default_user.rb
@@ -1,7 +1,5 @@
 class ChangeColumnDefaultUser < ActiveRecord::Migration[6.0]
   def change
-    reversible do |dir|
-      change_column_default :users, :bgcolor, '#005a55'
-    end
+    change_column_default :users, :bgcolor, '#005a55'
   end
 end
