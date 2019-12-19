4739830544917f3eaa10b90572903c60a8e18c05
rtnxs <riotnexus@mail.ru>
2019-12-19 15:51:12 +0300

    ref

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,13 +7,13 @@ class Question < ApplicationRecord
   validates :text, presence: true, length: {maximum: 255}
 
   before_save do
-    words = self.text.scan(/#[[:word:]]+/i)
-    words << self.answer.scan(/#[[:word:]]+/i) if self.answer
-    create_hashtags(words.flatten)
+    tags = self.text.scan(/#[[:word:]]+/i)
+    tags << self.answer.scan(/#[[:word:]]+/i) if self.answer
+    create_hashtags(tags.flatten.uniq)
   end
 
-  def create_hashtags(words)
-    words.uniq.map do |hastag|
+  def create_hashtags(tags)
+    tags.map do |hastag|
       tag = Hashtag.find_or_create_by(name: hastag.downcase.delete('#'))
       self.hashtags << tag
     end
