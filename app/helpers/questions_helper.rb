35f8a139961f3af54cd4a3ba49745856da76e12b
rtnxs <riotnexus@mail.ru>
2019-12-21 20:54:21 +0300

    refactor render_with_hashtag

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
   def render_with_hashtags(text)
-    text.gsub(HASHTAG_REGEXP){|tag| link_to tag, "/hashtags/#{tag.downcase.delete('#')}"}.html_safe
+    text.gsub(HASHTAG_REGEXP){|tag| link_to tag, hashtag_url(tag.downcase.delete('#'))}.html_safe
   end
 end
